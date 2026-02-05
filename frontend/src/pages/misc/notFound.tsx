e606ef6946eb8df1ce934d0242e9f7b56ae66f73
mazharulmaruf228 <43654422+mazharulmaruf228@users.noreply.github.com>
2026-02-06 00:07:46 +0600

    Add route protection and some error pages (#9)

---
+++ frontend/src/pages/misc/notFound.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+      <div className="w-96 mb-8">
+        <img
+          src="/assets/images/error_image.png"
+          alt="Not Found"
+          className="object-cover"
+        />
+      </div>
+
+      <h1 className="text-5xl! font-bold bg-linear-to-b from-gray-100 to-red-400 text-transparent bg-clip-text">
+        404
+      </h1>
+      <h2 className="font-bold mb-2">Page Not Found</h2>
+
+      <p className="text-center mb-4">
+        Sorry, the page you are looking for does not exist or has been moved.
+        <br />
+        Please check the URL or return to the{" "}
+        <Link to="/" className="underline text-foreground/80">
+          homepage
+        </Link>
+        .
+      </p>
+    </div>
+  );
+}
